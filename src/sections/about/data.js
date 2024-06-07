244852eed436e115cd2134e75771b1c85c899752
PabloSczn <sancheznarro.pablo@gmail.com>
2024-06-07 10:36:40 +0100

    Improved About me section

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -1,10 +1,24 @@
-import {FaAward} from 'react-icons/fa'
-import {TbBooks} from 'react-icons/tb'
-
+import { FaAward, FaCode, FaUsers } from 'react-icons/fa';
 
 const data = [
-    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '3+ Years Working'},
-    {id: 2, icon: <TbBooks/>, title: 'Projects', desc: '180+ Completed'},
-    ]
+    {
+        id: 1,
+        icon: <FaCode />,
+        title: 'Software Development',
+        desc: 'Passionate about developing cutting-edge software solutions.'
+    },
+    {
+        id: 2,
+        icon: <FaUsers />,
+        title: 'Team Player',
+        desc: 'Experienced in working with dynamic software development teams.'
+    },
+    {
+        id: 3,
+        icon: <FaAward />,
+        title: 'Experience',
+        desc: '3+ years of experience delivering high-quality, reliable solutions.'
+    }
+];
 
-    export default data;
+export default data;
