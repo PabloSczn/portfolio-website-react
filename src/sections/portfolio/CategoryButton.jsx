878350519bb562a7a3d26ad536746ed66bf0d1ac
PabloSczn <sancheznarro.pablo@gmail.com>
2024-07-07 16:42:23 +0100

    Added labels in projects

--- src/sections/portfolio/CategoryButton.jsx
+++ src/sections/portfolio/CategoryButton.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const CategoryButton = ({ category, className, onChangeCategory }) => {
   return (
-    <button className={className} onClick={() => onChangeCategory(category)}>{category}</button>
-  )
-}
+    <button className={className} onClick={() => onChangeCategory(category)} aria-label={`Filter projects by ${category}`}>
+      {category}
+    </button>
+  );
+};
 
-export default CategoryButton
+export default CategoryButton;
--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,21 +1,20 @@
-import Card from '../../components/Card'
+import Card from '../../components/Card';
 
-
-const Project = ({project}) => {
+const Project = ({ project }) => {
   return (
     <Card className="portfolio__project">
-        <div className="portfolio__project-image">
-            <img src={project.image} alt="Portfolio Project Image" />
-        </div>
-        <h4>{project.title}</h4>
-        <p>{project.desc}</p>
-        <div className="portfolio__project-cta">
-          <a href={project.demo} className="btn sm" target="_blank" rel="noopener noreferrer">Demo</a>
-          <a href={project.github} className="btn sm primary" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <div className="portfolio__project-icons">{project.icon}</div> {/* Displaying the icons */}
-        </div>
+      <div className="portfolio__project-image">
+        <img src={project.image} alt={`${project.title} project`} />
+      </div>
+      <h4>{project.title}</h4>
+      <p>{project.desc}</p>
+      <div className="portfolio__project-cta">
+        <a href={project.demo} className="btn sm" target="_blank" rel="noopener noreferrer" aria-label={`View demo of ${project.title}`}>Demo</a>
+        <a href={project.github} className="btn sm primary" target="_blank" rel="noopener noreferrer" aria-label={`View GitHub repository of ${project.title}`}>GitHub</a>
+        <div className="portfolio__project-icons">{project.icon}</div> {/* Displaying the icons */}
+      </div>
     </Card>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
