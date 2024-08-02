c974a27be4da5d752c9ab324dba9d2e99d1f9a66
PabloSczn <sancheznarro.pablo@gmail.com>
2024-08-02 03:09:02 +0100

    fixed contact form for phones

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -2,7 +2,7 @@ const data = [
     {id: 1, link: '#', title: 'Home'},
     {id: 2, link: '#about', title: 'About'},
     {id: 3, link: '#portfolio', title: 'Projects'},
-    {id: 3, link: '#testimonials', title: 'References'},
+    {id: 4, link: '#testimonials', title: 'References'},
     {id: 5, link: '#contact', title: 'Contact'}
     ]
 
